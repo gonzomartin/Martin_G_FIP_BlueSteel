75fdadbe39eea31dc0435759196941cb68047e8a
Gonzalo <gonzomartinarango@gmail.com>
2022-03-30 02:17:57 -0400

    Adding Header, Nav Menu and Banners

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+(function(){
+	"use strict";
+	console.log("fired");
+
+	const button = document.querySelector("#button");
+	const burgerMenu = document.querySelector(".burgerMenu");
+
+	function hamburgerMenu() {
+		//console.log("burger time!")
+		button.classList.toggle("expanded");
+		burgerMenu.classList.toggle("slideToggle");
+	}
+
+
+	button.addEventListener("click", hamburgerMenu);
+})();
