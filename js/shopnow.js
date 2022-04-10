4bf3472a41efa453235d998683c5be4f097fdbf7
Gonzalo <gonzomartinarango@gmail.com>
2022-04-10 13:15:08 -0400

    End of page 3

--- js/shopnow.js
+++ js/shopnow.js
@@ -4,6 +4,8 @@
 
 	const button = document.querySelector("#button");
 	const burgerMenu = document.querySelector(".burgerMenu");
+	const productCont = document.querySelectorAll('.productContainer'),
+				theBanner = document.querySelector('.bigPics');
 
 	function hamburgerMenu() {
 		//console.log("burger time!")
@@ -11,7 +13,14 @@
 		burgerMenu.classList.toggle("slideToggle");
 	}
 
+	function animateBanners () {
+    // figure out how much to move the banner images
+    theBanner.style.right = `${this.dataset.offset * 751}px`;
+  }
+
 
 	button.addEventListener("click", hamburgerMenu);
+	productCont.forEach(productCont => productCont.addEventListener('click', animateBanners));
 
+	animateBanners.call(productCont[0]);
 })();
